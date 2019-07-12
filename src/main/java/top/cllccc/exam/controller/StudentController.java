a6b33e939e20b9a7b25ca78f6abdbface09b6035
cchuanchuan <1393208441@qq.com>
2019-07-12 17:53:52 +0800

    Initial commit 1.0 完成所有pojo类，配置好数据库及分页拦截器

--- src/main/java/top/cllccc/exam/controller/StudentController.java
+++ src/main/java/top/cllccc/exam/controller/StudentController.java
@@ -1,6 +1,9 @@
 package top.cllccc.exam.controller;
 
 import com.github.pagehelper.PageHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +12,21 @@ import top.cllccc.exam.domain.Student;
 import top.cllccc.exam.service.StudentService;
 
 import java.util.List;
-
+//该注解相当于private static final Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
+@Slf4j
 @RequestMapping("/student")
 @Controller("studentController")
 public class StudentController {
     @Autowired
     private StudentService studentService;
 
+    //private static final Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
     @RequestMapping("allStudent")
     @ResponseBody
     public List<Student> getAllStudent(){
         PageHelper.startPage(2, 2);//pageNum 页码  pageSize 每页显示条数
         List<Student> students = studentService.getAllStudent();
+        log.info("获取所有student信息111");
         return students;
     }
 
